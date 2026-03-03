52564d22b887b132166ec2689d507854e8621705
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-03 14:06:15 +0530

    q2

--- Weekly_Assignments.java
+++ Weekly_Assignments.java
@@ -1,7 +1,7 @@
-gitpublic class Weekly_Assignments {
+public class Weekly_Assignments {
 	public static void main(String[] args) {
 		int p=95,c=96,m=94;
 		double avg=(p+c+m)/3;
-		System.out.println("Sam's average mark in PCM is "+avg);go
+		System.out.println("Sam's average mark in PCM is "+avg);
 	}
 }
