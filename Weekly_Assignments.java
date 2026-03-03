39c9b7fc9dbd3d6365f370005499c770a40dd3fb
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-03 14:14:22 +0530

    q3

--- Weekly_Assignments.java
+++ Weekly_Assignments.java
@@ -1,7 +1,8 @@
 public class Weekly_Assignments {
 	public static void main(String[] args) {
-		int p=95,c=96,m=94;
-		double avg=(p+c+m)/3;
-		System.out.println("Sam's average mark in PCM is "+avg);
+
+		double km=10.8,mile;
+		mile=10.8*1.6;
+		System.out.println("The distance " +km+ "km in miles is "+mile);
 	}
 }
