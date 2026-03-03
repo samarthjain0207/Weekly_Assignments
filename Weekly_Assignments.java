14cb42d80b03a44b1dff46bc74b029d763d4f76e
samarthjain0207 <samarthjain7020@gmail.com>
2026-03-03 14:03:49 +0530

    q2 completed

--- Weekly_Assignments.java
+++ Weekly_Assignments.java
@@ -1,12 +1,8 @@
 import java.util.Scanner;
 public class Weekly_Assignments {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Your Birth Year: ");
-		int bdyear=scanner.nextInt();
-		System.out.println("Enter Current Year: ");
-		int year=scanner.nextInt();
-		int age=year-bdyear;
-		System.out.println("Age: "+age);
+		int p=95,c=96,m=94;
+		double avg=(p+c+m)/3;
+		System.out.println("Sam's average mark in PCM is "+avg);go
 	}
 }
